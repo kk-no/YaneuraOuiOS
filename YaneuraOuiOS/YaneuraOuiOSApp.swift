692e1fffb147884b50f0a511607d9dd1080ab22a
select766 <select766@outlook.jp>
2022-10-08 16:56:59 +0900

    Initial Commit

---
+++ YaneuraOuiOS/YaneuraOuiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  YaneuraOuiOSApp.swift
+//  YaneuraOuiOS
+//
+//  Created by Masatoshi Hidaka on 2022/10/08.
+//
+
+import SwiftUI
+
+@main
+struct YaneuraOuiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
